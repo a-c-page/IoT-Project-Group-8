27efd24a1d18922be7f7949f2472d9635b3f8a40
Austin <austincolepage@hotmail.com>
2021-12-02 02:22:05 -0500

    make it better

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-tvbn7n%^67+$uv-ac(^4ukt@jm*xhrvre@gwrlz#y^o)1nqr7t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
